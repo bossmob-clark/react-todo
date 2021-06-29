64f4a61861b622530372c0eaa5028ad72ad03496
clark <>
2021-06-30 07:20:39 +0900

    - 기본 문법공부 - 라우터 세팅중

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import Root from './client/Root';
+// import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+
+
+// ========================================
+
+ReactDOM.render(
+    <Root/>,
+    document.getElementById('root')
+);
